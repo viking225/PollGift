f292b18e774984c4c86e01b1bc3e6bbe9f2e6684
viking225 <firstviking225@gmail.com>
2016-10-26 02:17:45 +0200

    Git Ignore update

--- config/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Tanoh Kevin on 25/09/2016.
- */
-module.exports = {
-    commands: require('./commands'),
-    database: require('./database'),
-    default: require('./default')
-};
